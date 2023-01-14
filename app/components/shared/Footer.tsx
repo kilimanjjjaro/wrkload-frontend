929e673c9679882555dc777ebb403ee81e8eba8d
Kilimanjjjaro <hola@kilimanjjjaro.com>
2023-01-14 18:42:52 -0300

    Fixes on nav and footer

--- app/components/shared/Footer.tsx
+++ app/components/shared/Footer.tsx
@@ -3,7 +3,7 @@ import Languages from 'app/components/shared/Languages'
 
 export default function Footer (): JSX.Element {
   return (
-    <footer className='fixed bottom-0 left-0 z-50 flex items-end justify-between w-full px-6 py-6 mx-auto text-dark-gray md:py-8 md:px-8'>
+    <footer className='fixed bottom-0 left-0 z-50 flex items-end justify-between w-full px-6 pb-6 mx-auto text-dark-gray md:pb-8 md:px-8'>
       <span className='text-sm font-secondaryFont'>
         © 2023 wrkload. All rights reserved.
       </span>
