7ae75248f3ef2b1e6e0b32b2782e4009c69831dd
Kilimanjjjaro <hola@kilimanjjjaro.com>
2023-01-25 22:20:43 -0300

    Metatags and hero header updated

--- app/components/HeroHeader.tsx
+++ app/components/HeroHeader.tsx
@@ -14,7 +14,7 @@ export default function HeroHeader (): JSX.Element {
       <div className='flex flex-col items-start text-white justify-cente'>
         <Headline variant='2xl'>Work, load your <br />time and go <br />have fun!</Headline>
         <div className='mb-8 md:w-1/2'>
-          <Paragraph variant='normal'><b className='font-extrabold'>Easy-peasy!</b><br /> With wrkload you can track what you work on in a simple and organized way.</Paragraph>
+          <Paragraph variant='normal'><b className='font-extrabold'>Easy-peasy!</b><br /> With wrkload you can track what you&apos;ve worked on in a simple and organized way.</Paragraph>
         </div>
         <Button variant='primary' onClick={() => router.push('/registry')}>Start now for free <ArrowRightIcon className='w-4 stroke-3' /></Button>
       </div>
