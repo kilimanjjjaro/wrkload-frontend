23fd3578968e172e4bf995bc3ff1813ca976d1eb
Kilimanjjjaro <hola@kilimanjjjaro.com>
2023-03-02 01:49:26 -0300

    Add OP metas

--- app/head.tsx
+++ app/head.tsx
@@ -2,6 +2,17 @@ export default function Head (): JSX.Element {
   return (
     <>
       <title>wrkload</title>
+      <meta name='viewport' content='width=device-width, initial-scale=1' />
+      <link href='/favicon.png' rel='shortcut icon' />
+      <meta property='og:title' content='wrkload' />
+      <meta property='og:description' content="wrkload is a web app where you can track what you've worked on in a simple and organized way." />
+      <meta property='og:url' content='https://wrkload.vercel.app/' />
+      <meta property='og:site_name' content='wrkload' />
+      <meta property='og:locale' content='en-US' />
+      <meta property='og:image:url' content='https://wrkload.vercel.app/og.png' />
+      <meta property='og:image:width' content='800' />
+      <meta property='og:image:height' content='600' />
+      <meta property='og:type' content='website' />
     </>
   )
 }
