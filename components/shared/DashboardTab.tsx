dba8a39ff6c3cb9826b72404ba9ee6200b6bb241
Kilimanjjjaro <hola@kilimanjjjaro.com>
2023-03-14 16:25:46 -0300

    Navbar improvements

--- components/shared/DashboardTab.tsx
+++ components/shared/DashboardTab.tsx
@@ -58,20 +58,20 @@ export default function DashboardTab ({ showDashboardBox, setShowDashboardBox }:
 
   return (
     <motion.nav
-      className='fixed bottom-0 flex-col items-center w-full h-full gap-3 md:mt-8 md:h-auto md:absolute md:top-0 md:w-auto justify-self-center'
+      className='fixed bottom-0 flex-col items-center justify-center w-full h-full gap-6 xl:gap-3 md:mt-8 md:h-auto md:absolute md:top-0 md:w-auto justify-self-center'
       animate={showDashboardBox ? 'open' : 'closed'}
       variants={VARIANTS}
       transition={{ ease: 'easeInOut', duration: 0.4 }}
       initial={false}
     >
       <div className='fixed w-full block xl:hidden h-full -z-10 bg-light-blue dark:bg-black opacity-[0.97]' />
-      <ul className='flex flex-col items-center justify-center w-full h-full gap-6 text-3xl text-black md:gap-4 md:text-xl md:flex-row md:w-auto rounded-t-3xl md:rounded-3xl font-secondaryFont'>
+      <ul className='flex flex-col items-center justify-center w-full gap-6 text-3xl text-black md:gap-4 md:text-xl md:flex-row md:w-auto rounded-t-3xl md:rounded-3xl font-secondaryFont'>
         {pages.map((page) => (
           <li className='px-8 py-4 transition ease-in-out rounded-full cursor-pointer md:px-6 md:py-2 bg-blue duration-400 md:rounded-3xl md:hover:bg-black md:hover:text-white dark:md:hover:text-black dark:md:hover:bg-white' key={page.link} onClick={() => setShowDashboardBox(!showDashboardBox)}><Link href={page.link}>{page.name}</Link></li>
         ))}
         <button className='px-8 py-4 transition ease-in-out rounded-full cursor-pointer md:px-5 md:py-2 bg-blue duration-400 md:rounded-3xl md:hover:bg-black md:hover:text-white dark:md:hover:text-black dark:md:hover:bg-white' onClick={() => { void handleLogout() }}>Log out</button>
       </ul>
-      <div className='p-2 mb-10 transition xl:absolute xl:mb-0 xl:mt-4 xl:top-full ease-in-out rounded-full cursor-pointer w-9 md:w-7 bg-blue duration-400 md:hover:bg-black [&>svg]:dark:md:hover:stroke-black [&>svg]:md:hover:stroke-white dark:md:hover:bg-white' onClick={() => setShowDashboardBox(!showDashboardBox)}>
+      <div className='p-2 transition xl:absolute xl:mt-4 xl:top-full ease-in-out rounded-full cursor-pointer w-9 md:w-7 bg-blue duration-400 md:hover:bg-black [&>svg]:dark:md:hover:stroke-black [&>svg]:md:hover:stroke-white dark:md:hover:bg-white' onClick={() => setShowDashboardBox(!showDashboardBox)}>
         <XMarkIcon className='transition ease-in-out stroke-2 stroke-black duration-400' />
       </div>
     </motion.nav>
