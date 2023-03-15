7fde88813473c625210e4529ebce8bb36dc0f1fa
Kilimanjjjaro <hola@kilimanjjjaro.com>
2023-03-14 21:52:32 -0300

    Refresh access token improvements

--- services/getAccessToken.ts
+++ services/getAccessToken.ts
@@ -1,9 +1,16 @@
+import { setCookie } from 'cookies-next'
 import api from 'utils/api'
 
 export default async function getAccessToken (): Promise<void> {
   const response = await api.get('/auth/refreshToken')
 
   console.log(response.data)
+
+  setCookie('accessToken', response.data.accessToken, {
+    maxAge: response.data.expiresIn,
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: 'strict'
+  })
 
   return response.data
 }
