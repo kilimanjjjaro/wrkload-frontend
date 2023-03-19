c9f2b9d2c86ef7a484aff16a8300fe3181e60ed0
Kilimanjjjaro <hola@kilimanjjjaro.com>
2023-03-19 01:26:16 -0300

    Add light styles

--- components/tasks/Header.tsx
+++ components/tasks/Header.tsx
@@ -11,7 +11,7 @@ export default function Header ({ projectNames, shouldRenderNotFound }: Props): 
     <header className='flex flex-col justify-between mb-10 text-black xl:flex-row dark:text-blue gap-y-10'>
       <h2 className='flex flex-col text-6xl md:flex-row gap-x-5 font-primaryFont'>
         <span className='transition-colors ease-in-out duration-400'>Tasks of</span>
-        {projectNames.length === 0 && <div className='w-56 rounded-full bg-gradient-to-r from-light-blue via-blue to-light-blue bg-[length:200%_100%] h-14 animate-skeleton' />}
+        {projectNames.length === 0 && <div className='w-56 rounded-full bg-gradient-to-r from-white dark:from-light-blue via-blue to-light-blue dark:to-light-blue bg-[length:200%_100%] h-14 animate-skeleton' />}
         {projectNames.length >= 1 && (
           <ProjectSelector projectNames={projectNames} />
         )}
