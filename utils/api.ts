2953ef7c68c1e94b8c8dd0a16056dcdbe14de844
Kilimanjjjaro <hola@kilimanjjjaro.com>
2023-03-18 21:43:47 -0300

    update axios settings

--- utils/api.ts
+++ utils/api.ts
@@ -1,5 +1,7 @@
 import axios from 'axios'
 
+axios.defaults.withCredentials = true
+
 const BASE_URL = `${process.env.NODE_ENV === 'production' ? 'https://wrkload-api-production.up.railway.app' : 'http://localhost:5000'}/api/v1`
 
 const api = axios.create({
