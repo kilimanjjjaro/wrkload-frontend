18f1f459733a13ce92e8935b2a831e4e5c741c09
Kilimanjjjaro <hola@kilimanjjjaro.com>
2023-03-18 22:12:58 -0300

    Delete withCredentials

--- utils/api.ts
+++ utils/api.ts
@@ -1,7 +1,5 @@
 import axios from 'axios'
 
-axios.defaults.withCredentials = true
-
 const BASE_URL = `${process.env.NODE_ENV === 'production' ? 'https://wrkload-api-production.up.railway.app' : 'http://localhost:5000'}/api/v1`
 
 const api = axios.create({
