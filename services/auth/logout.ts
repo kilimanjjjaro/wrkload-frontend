8951a48d388808faf89ded25f3e24a63e436ea60
Kilimanjjjaro <hola@kilimanjjjaro.com>
2023-04-14 17:54:34 -0300

    Add notification in logout

--- services/auth/logout.ts
+++ services/auth/logout.ts
@@ -1,11 +1,17 @@
 import { deleteCookie } from 'cookies-next'
+import { toast } from 'sonner'
 import { mutate } from 'swr'
 import api from 'utils/api'
 
 export default async function logout (): Promise<void> {
-  await api.get('/auth/logout')
-  await mutate(() => true, undefined, { revalidate: false })
-  window.localStorage.removeItem('user')
-  deleteCookie('accessToken')
-  deleteCookie('isLogged')
+  try {
+    await api.get('/auth/logout')
+    await mutate(() => true, undefined, { revalidate: false })
+    window.localStorage.removeItem('user')
+    deleteCookie('accessToken')
+    deleteCookie('isLogged')
+    toast.success('Session ended successfully')
+  } catch (error) {
+    toast.error('Something went wrong. Please, try again!')
+  }
 }
