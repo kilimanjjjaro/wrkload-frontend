2ebc6dc83e435c3adfe1edf4945ee27bc0cdc69e
Kilimanjjjaro <hola@kilimanjjjaro.com>
2023-04-19 11:37:25 -0300

    Improve users page redirection

--- app/(dashboard)/users/page.tsx
+++ app/(dashboard)/users/page.tsx
@@ -1,9 +1,8 @@
 'use client'
 
 import { useEffect } from 'react'
-import { useRouter, useSearchParams } from 'next/navigation'
+import { useSearchParams } from 'next/navigation'
 import useSWR from 'swr'
-import jwtDecode from 'jwt-decode'
 import Header from 'components/users/Header'
 import Skeleton from 'components/users/Skeleton'
 import UserList from 'components/users/UserList'
@@ -12,25 +11,12 @@ import Modals from 'components/users/Modals'
 import PageTransition from 'components/shared/PageTransition'
 import { getUsers } from 'services/users/getUsers'
 import { sortUsers } from 'utils/sortData'
-import { getCookie } from 'cookies-next'
 
 export default function Users (): JSX.Element {
-  const router = useRouter()
   const params = useSearchParams()
   const page = params.get('page')
-  const accessToken = getCookie('accessToken')
 
   const { data, isLoading, isValidating, mutate } = useSWR('users', async () => await getUsers({ page }), { onSuccess: data => sortUsers(data.users) })
-
-  useEffect(() => {
-    if (accessToken !== undefined) {
-      const { role }: { role: number } = jwtDecode(accessToken as string)
-
-      if (role !== 1) {
-        router.push('/tasks')
-      }
-    }
-  }, [accessToken])
 
   useEffect(() => {
     mutate().catch((error) => console.error(error))
--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,20 @@
+import jwtDecode from 'jwt-decode'
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export function middleware (request: NextRequest): NextResponse {
   const isLogged = request.cookies.get('isLogged')
+  const accessToken = request.cookies.get('accessToken')
 
   if (isLogged === undefined) return NextResponse.redirect(new URL('/', request.url))
+
+  if (request.nextUrl.pathname.startsWith('/users')) {
+    if (accessToken !== undefined) {
+      const { role }: { role: number } = jwtDecode(accessToken.value)
+
+      if (role !== 1) return NextResponse.redirect(new URL('/tasks', request.url))
+    }
+  }
 
   return NextResponse.next()
 }
