8dc790af72dc2619cf91cbf96bbe8a871b2dd7a4
Kilimanjjjaro <hola@kilimanjjjaro.com>
2023-06-03 11:51:22 -0300

    Change API URL

--- utils/api.ts
+++ utils/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 axios.defaults.withCredentials = true
 
-const BASE_URL = `${process.env.NODE_ENV === 'production' ? 'https://wrkload-api-production.up.railway.app' : 'http://localhost:5000'}/api/v1`
+const BASE_URL = `${process.env.NODE_ENV === 'production' ? 'https://wrkload-api.onrender.com' : 'http://localhost:5000'}/api/v1`
 
 const api = axios.create({
   baseURL: BASE_URL
--- utils/privateApi.ts
+++ utils/privateApi.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosResponse } from 'axios'
 import { getCookie, setCookie } from 'cookies-next'
 
-const BASE_URL = `${process.env.NODE_ENV === 'production' ? 'https://wrkload-api-production.up.railway.app' : 'http://localhost:5000'}/api/v1`
+const BASE_URL = `${process.env.NODE_ENV === 'production' ? 'https://wrkload-api.onrender.com' : 'http://localhost:5000'}/api/v1`
 
 interface RefreshTokenInterface {
   status: string
